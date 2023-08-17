31c408ccfff1959880c6e04f3b741b33ac283b61
Raul Effting <rauleffting94@gmail.com>
2023-08-17 19:18:05 -0300

    feat: add POST to /users endpoint

---
+++ src/http/controllers/register.ts
@@ -0,0 +1,27 @@
+import { type FastifyRequest, type FastifyReply } from 'fastify'
+import { hash } from 'bcryptjs'
+import { prisma } from '@/lib/prisma'
+import { z } from 'zod'
+import { registerUseCase } from '@/use-cases/register'
+
+export async function register(request: FastifyRequest, reply: FastifyReply) {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
+
+  const { name, email, password } = registerBodySchema.parse(request.body)
+
+  try {
+    await registerUseCase({
+      name,
+      email,
+      password,
+    })
+  } catch (err) {
+    return reply.status(409).send()
+  }
+
+  return await reply.status(201).send()
+}
