960547962ff0ba252d425014f95ec8766b1fec0b
Raul Effting <rauleffting94@gmail.com>
2023-08-17 23:44:46 -0300

    refactor: change register use-case to avoid prisma dependencies

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,5 +1,7 @@
 import { prisma } from '@/lib/prisma'
+import { UsersRepository } from '@/repositories/users-repository'
 import { hash } from 'bcryptjs'
+import { UserAlreadyExistsError } from './errors/user-already-exists-error'
 
 interface RegisterUseCaseRequest {
   name: string
@@ -7,28 +9,22 @@ interface RegisterUseCaseRequest {
   password: string
 }
 
-export async function registerUseCase({
-  name,
-  email,
-  password,
-}: RegisterUseCaseRequest) {
-  const passwordHash = await hash(password, 6)
+export class RegisterUseCase {
+  constructor(private usersRepository: UsersRepository) {}
 
-  const userWithSameEmail = await prisma.user.findUnique({
-    where: {
-      email,
-    },
-  })
+  async execute({ name, email, password }: RegisterUseCaseRequest) {
+    const passwordHash = await hash(password, 6)
 
-  if (userWithSameEmail) {
-    throw new Error('E-mail already exists.')
-  }
+    const userWithSameEmail = await this.usersRepository.findByEmail(email)
 
-  await prisma.user.create({
-    data: {
+    if (userWithSameEmail) {
+      throw new UserAlreadyExistsError()
+    }
+
+    await this.usersRepository.create({
       name,
       email,
       password_hash: passwordHash,
-    },
-  })
+    })
+  }
 }
