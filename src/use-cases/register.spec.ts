0a9239b97e2b99f3a194af4bdf60e4488ab9e114
Raul Effting <rauleffting94@gmail.com>
2023-08-19 22:08:52 -0300

    test: ensure user password is hashed

---
+++ src/use-cases/register.spec.ts
@@ -0,0 +1,23 @@
+import { expect, describe, it } from 'vitest'
+import { RegisterUseCase } from './register'
+import { compare } from 'bcryptjs'
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
+
+describe('Register Use Case', () => {
+  it('should hash user password upon registration', async () => {
+    const usersRepository = new InMemoryUsersRepository()
+    const registerUseCase = new RegisterUseCase(usersRepository)
+
+    const { user } = await registerUseCase.execute({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    const isPasswordCorrectlyHashed = await compare(
+      '123456',
+      user.password_hash,
+    )
+    expect(isPasswordCorrectlyHashed).toBe(true)
+  })
+})
