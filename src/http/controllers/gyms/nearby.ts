0c6222e21833476e5cf03c3f386ba281fdbd9950
Raul Effting <rauleffting94@gmail.com>
2023-09-08 23:14:27 -0300

    fix: add coerce to validate latitude and longitude on nearby controller

--- src/http/controllers/gyms/nearby.ts
+++ src/http/controllers/gyms/nearby.ts
@@ -1,13 +1,13 @@
-import { type FastifyRequest, type FastifyReply } from 'fastify'
+import { FastifyReply, FastifyRequest } from 'fastify'
 import { z } from 'zod'
 import { makeFetchNearbyGymsUseCase } from '@/use-cases/factories/make-fetch-nearby-gyms-use-case'
 
 export async function nearby(request: FastifyRequest, reply: FastifyReply) {
   const nearbyGymsQuerySchema = z.object({
-    latitude: z.number().refine((value) => {
+    latitude: z.coerce.number().refine((value) => {
       return Math.abs(value) <= 90
     }),
-    longitude: z.number().refine((value) => {
+    longitude: z.coerce.number().refine((value) => {
       return Math.abs(value) <= 180
     }),
   })
@@ -21,5 +21,7 @@ export async function nearby(request: FastifyRequest, reply: FastifyReply) {
     userLongitude: longitude,
   })
 
-  return await reply.status(200).send({ gyms })
+  return reply.status(200).send({
+    gyms,
+  })
 }
